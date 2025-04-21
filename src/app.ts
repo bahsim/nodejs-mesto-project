f269fd9d42d52ca143cb196df2fa3dfb2d66bdae
Mikhail Burnin <bahsim@mail.ru>
2025-04-21 09:52:37 +0500

    Implemented API Mesto

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,20 @@
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
 import mongoose from 'mongoose';
-import userRouter from './routes/user';
-import cardRouter from './routes/card';
-import { errorHandler } from './middlewares/error-handler';
+import router from './routes/index';
 
 const app = express();
 
 app.use(express.json());
+app.use(router);
 
 mongoose.connect('mongodb://localhost:27017/mestodb')
   .then(() => {
     console.log('Connected to MongoDB');
+
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
   })
   .catch((err) => {
     console.log('Error connecting to MongoDB', err);
   });
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  // @ts-ignore
-  req.user = {
-    _id: '68035871ec0fe55b8498687c',
-  };
-  next();
-});
-
-app.use(userRouter);
-app.use(cardRouter);
-
-app.use(errorHandler);
-
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
